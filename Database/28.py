4debce6e18a7ca1a6ce9b859a9c77138d25ede08
Vlad <vlad.sarana04@gmail.com>
2022-09-10 12:22:32 +0200

    done №28

--- Database/28.py
+++ Database/28.py
@@ -5,5 +5,36 @@ from typing import List
 
 db_pass = os.path.join(os.getcwd(), 'chinook.db')
 connection = sqlite3.connect(db_pass)
+# print(db_pass)
 
-print(db_pass)
+def execute_query(connection, 
+                  query_sql: str) -> List:
+    '''
+    Функція для виповнення запроса
+    :param з'єднання з бд, sql-запрос
+    :return: результат виповнення запроса
+    '''
+    cur = connection.cursor()
+    result = cur.execute(query_sql)
+    return result
+
+def unwrapp_query_result(records: List) -> None:
+    '''
+    Функція для красивого вивода результату в консоль
+    :param список строк
+    '''
+    for record in records:
+        print(*record)
+
+def get_invoice_items() -> None:
+    '''
+    d
+    '''
+    query_sql = f'''
+            SELECT sum(UnitPrice * Quantity)
+                FROM invoice_items;
+    '''
+    result = execute_query(connection, query_sql)
+    unwrapp_query_result(result)
+
+get_invoice_items()
